33416e13f653ac7853c88529984d165f18152101
AmnaBatul <amnabatul2@gmail.com>
2025-02-16 21:27:33 +0500

    CaptureActivity added

---
+++ app/src/main/java/com/example/assignment_1/CaptureActivity.kt
@@ -0,0 +1,31 @@
+package com.example.assignment_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_15)
+
+
+        val closeButton = findViewById<ImageView>(R.id.closeButton)
+        val nextText = findViewById<TextView>(R.id.nextText)
+
+
+        closeButton.setOnClickListener {
+            finish()
+        }
+
+        nextText.setOnClickListener{
+            val intent = Intent(this, NewPostActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
