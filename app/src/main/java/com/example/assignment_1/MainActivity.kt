9cac800a68f3225dd7f5af831f34a0913f194493
AmnaBatul <amnabatul2@gmail.com>
2025-02-16 21:33:52 +0500

    Login main and register activities added

---
+++ app/src/main/java/com/example/assignment_1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.assignment_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_0)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
