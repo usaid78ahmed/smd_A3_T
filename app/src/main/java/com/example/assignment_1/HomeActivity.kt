96fe3c3cb0148d62f31d5b88174f5ea01e87ae05
AmnaBatul <amnabatul2@gmail.com>
2025-02-16 21:32:42 +0500

    HomeActivity modified

--- app/src/main/java/com/example/assignment_1/HomeActivity.kt
+++ app/src/main/java/com/example/assignment_1/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.assignment_1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -9,6 +11,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)
+
+        // Navigate to DMActivity
+        val dmicon = findViewById<ImageView>(R.id.dmicon)
+        dmicon.setOnClickListener {
+            val intent = Intent(this, DMActivity::class.java)
+            startActivity(intent)
+        }
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.selectedItemId = R.id.nav_home
@@ -27,6 +36,11 @@ class HomeActivity : AppCompatActivity() {
                 }
                 R.id.nav_contacts -> {
                     startActivity(Intent(this, ContactsActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_add -> {
+                    startActivity(Intent(this, NewPostActivity::class.java))
                     overridePendingTransition(0, 0)
                     return@setOnItemSelectedListener true
                 }
